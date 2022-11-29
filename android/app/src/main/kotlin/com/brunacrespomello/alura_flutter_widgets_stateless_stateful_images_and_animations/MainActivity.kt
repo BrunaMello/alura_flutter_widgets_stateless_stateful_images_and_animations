9b2e78941a61fe33f2878d37c7efabfdf8598ce8
brunamello <brunacrespomello@gmail.com>
2022-11-29 05:11:37 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/brunacrespomello/alura_flutter_widgets_stateless_stateful_images_and_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.brunacrespomello.alura_flutter_widgets_stateless_stateful_images_and_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
